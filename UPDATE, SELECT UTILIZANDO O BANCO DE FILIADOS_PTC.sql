3f202e87bf57535855d95ab8f463d892b457d9ec
RenanNeets <130794999+RenanNeets@users.noreply.github.com>
2023-04-14 17:10:50 -0300

    Update UPDATE, SELECT UTILIZANDO O BANCO DE FILIADOS_PTC.sql
    Second update

--- UPDATE, SELECT UTILIZANDO O BANCO DE FILIADOS_PTC.sql
+++ UPDATE, SELECT UTILIZANDO O BANCO DE FILIADOS_PTC.sql
@@ -2,82 +2,119 @@ CREATE DATABASE bdPartidoPTC
 USE bdPartidoPTC
 
 SELECT * FROM filiadosPTC
---Questão 1
+--QuestÃ£o 1
 
 SELECT nomeFiliado, UF FROM filiadosPTC
  WHERE nomeFiliado LIKE 'ANTONIO%'
 
---Questão 2 ARRUMAR
+--QuestÃ£o 2 ARRUMAR
 
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE nomeFiliado LIKE '%SEBASTIAO%'
 
---Questão 3
+--QuestÃ£o 3
 
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE nomeFiliado LIKE 'OS%'
 
---Questão 4
+--QuestÃ£o 4
 SELECT nomeFiliado, nomeMunicipio FROM filiadosPTC
 	WHERE nomeMunicipio LIKE '%LIX%'
---Questão 5
+--QuestÃ£o 5
 SELECT nomeFiliado, nomeMunicipio FROM filiadosPTC
 	WHERE nomeMunicipio LIKE '%INHAMBUPE%'
---Questão 6
+--QuestÃ£o 6
 SELECT nomeMunicipio FROM filiadosPTC
 	WHERE nomeMunicipio LIKE 'SANTA%'
---Questão 7
+--QuestÃ£o 7
 SELECT nomeMunicipio FROM filiadosPTC
 	WHERE nomeMunicipio LIKE 'E%'
---Questão 8
+--QuestÃ£o 8
 SELECT nomeMunicipio FROM filiadosPTC
 	WHERE nomeMunicipio LIKE '%CRUZ%'
---Questão 9
+--QuestÃ£o 9
 SELECT nomeMunicipio FROM filiadosPTC
 	WHERE nomeMunicipio LIKE '%DE%'
---Questão 10
+--QuestÃ£o 10
 SELECT dataFiliacao FROM filiadosPTC
 	WHERE dataFiliacao >'01/01/1990' AND dataFiliacao <'01/01/2000'
---Questão 11
+--QuestÃ£o 11
 SELECT dataFiliacao FROM filiadosPTC
 	WHERE dataFiliacao >'02/01/2010' AND dataFiliacao <'01/03/2010'
---Questão 12
+--QuestÃ£o 12
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE dataFiliacao >'01/07/2007' AND dataFiliacao <'01/07/2007'
---Questão 13
+--QuestÃ£o 13
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE situacaoRegistro LIKE 'REGULAR'
---Questão 14
+--QuestÃ£o 14
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE zonaEleitoral LIKE '203'
---Questão 15
+--QuestÃ£o 15
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE zonaEleitoral LIKE '180'
---Questão 16
+--QuestÃ£o 16
 SELECT nomeFiliado FROM filiadosPTC
 	WHERE zonaEleitoral LIKE '199'
---Questão 17
+--QuestÃ£o 17
 SELECT MAX(zonaEleitoral) FROM filiadosPTC
---Questão 18
+--QuestÃ£o 18
 SELECT MIN(zonaEleitoral) FROM filiadosPTC
---Questão 19
+--QuestÃ£o 19
 UPDATE filiadosPTC
 	SET siglaPartido = 'PT'
 		WHERE codigoMunicipio LIKE '36471'
---Questão 20
+--QuestÃ£o 20
 UPDATE filiadosPTC
 	SET siglaPartido = 'PMDB'
 		WHERE codigoMunicipio LIKE '38490'
---Questão 21
+--QuestÃ£o 21
 UPDATE filiadosPTC
 	SET siglaPartido = 'PC'
-	WHERE nomeMunicipio LIKE 'SANTAN%' AND nomeMunicipio LIKE'%POLIS'
---Questão 22
---Questão 23
---Questão 24
---Questão 25
---Questão 26
---Questão 27
---Questão 28
---Questão 29
---Questão 30
+		WHERE nomeMunicipio LIKE 'SANTAN%' AND nomeMunicipio LIKE'%POLIS'
+--QuestÃ£o 22
+UPDATE filiadosPTC
+	SET siglaPartido = 'PV', nomePartido = 'Partido Verde dos Filiados'
+		WHERE nomeMunicipio = 's%' AND nomeMunicipio  LIKE '%LIX'
+
+
+--QuestÃ£o 23
+UPDATE filiadosPTC
+	SET siglaPartido = 'PSDB', nomePartido = ' Partido da Social Democracia Brasileira'
+		WHERE nomeMunicipio = 'ENCRUZILHADA'
+
+--QuestÃ£o 24
+UPDATE filiadosPTC
+	SET nomeFiliado = 'JORGE LUIS MAGALHÃƒES DOS SANTOS'
+		WHERE nomeFiliado = 'JORGE LUIS FERREIRA DOS SANTOS'
+
+
+--QuestÃ£o 25
+UPDATE filiadosPTC
+	SET nomeFiliado = 'RENATA XAVIER RODRIGUES'
+		WHERE nomeFiliado = 'BENTA XAVIER RODRIGUES'
+
+--QuestÃ£o 26
+UPDATE filiadosPTC
+	SET nomeFiliado = 'NOEL LEITE DA SILVA'
+		WHERE nomeFiliado = 'NOE LEITE DA SILVA'
+
+--QuestÃ£o 27
+UPDATE filiadosPTC
+	SET situacaoRegistro = 'REGULAR'
+		WHERE nomeFiliado = 'JOSIENE ANDRADE DE SOUZA'
+
+--QuestÃ£o 28
+UPDATE filiadosPTC
+	SET situacaoRegistro = 'REGULAR'
+		WHERE nomeFiliado = 'ELIEL ALMEIDA SILVA'
+
+--QuestÃ£o 29
+UPDATE filiadosPTC
+	SET situacaoRegistro = 'DESFILIADO'
+		WHERE nomeFiliado = 'ALINE DE SOUZA'
+
+--QuestÃ£o 30
+ALTER TABLE filiadosPTC
+	ADD Sexo CHAR(1);
+
